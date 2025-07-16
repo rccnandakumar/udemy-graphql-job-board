97a58b9bf03a33b3f5c30c63d3a0dd66f9e20d39
rccnandakumar <rccnandakumar@gmail.com>
2025-07-16 22:56:53 +0530

    Data Loaders

--- server/resolver.js
+++ server/resolver.js
@@ -34,7 +34,9 @@ export const resolvers = {
 
   Job: {
     date: (job) => toIsoDate(job.createdAt),
-    company: (job) => getCompany(job.companyId),
+    company: (job, _args, { companyLoader }) => {
+      return companyLoader.load(job.companyId);
+    },
   },
 
   Mutation: {
